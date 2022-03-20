10759f210a97a953562d86d8e9b7a0cd3ccdaa7a
Arianna Salerno <ariannasalerno00@icloud.com>
2022-03-20 22:51:50 +0100

    added methods for modular arithmetic’s calculation

--- code/src/main/java/it/polimi/ingsw/misc/Utils.java
+++ code/src/main/java/it/polimi/ingsw/misc/Utils.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.misc;
 
+import it.polimi.ingsw.model.IslandGroup;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -11,6 +13,26 @@ public class Utils {
 
         int randomNumber = random.nextInt(list.size());
         return list.get(randomNumber);
-
     }
+
+
+    public static <T> T modularSelection(T element, ArrayList<T> group, int movement){
+        int index = group.indexOf(element);
+
+        int nextPosition = modularSum(index, movement);
+
+        return group.get(nextPosition);
+    }
+
+    public static int modularSum(int currentPos, int movement){
+
+        int sum = currentPos + movement;
+        if (sum > 12) {
+            sum = sum - 12;
+        }
+        return sum;
+    }
+
+
+
 }
