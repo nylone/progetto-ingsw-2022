b23e806ca21ee270a80f66115c5ca4e791dd96a5
Alessandro Rossi <alessandro33.rossi@mail.polimi.it>
2022-03-24 15:13:19 +0100

    updated CharacterCard class

--- code/src/main/java/it/polimi/ingsw/Model/CharacterCard.java
+++ code/src/main/java/it/polimi/ingsw/Model/CharacterCard.java
@@ -4,9 +4,16 @@ import java.io.Serializable;
 
 abstract class CharacterCard implements Serializable {
     private int id;
-    //GameBoard context;
     private Character character;
     private int cost;
     private int timeUsed;
 
+    public CharacterCard(Character character, int cost){
+        this.character = character;
+        this.cost = cost;
+    }
+    public abstract int getId();
+    public abstract Character getCharacter();
+    public abstract int getCost();
+    public abstract int getTimeUsed();
 }
