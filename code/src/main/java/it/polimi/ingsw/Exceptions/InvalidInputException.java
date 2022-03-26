c3a03eee40a90d644e2dcf3f3db5c6b898c609d8
Arianna Salerno <ariannasalerno00@icloud.com>
2022-03-26 13:30:12 +0100

    changed to RuntimeException, added custom message method overload

--- code/src/main/java/it/polimi/ingsw/Exceptions/InvalidInputException.java
+++ code/src/main/java/it/polimi/ingsw/Exceptions/InvalidInputException.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.Exceptions;
 
-public class InvalidInputException extends Exception {
+public class InvalidInputException extends RuntimeException {
     public InvalidInputException() {
         super("Invalid input provided");
     }
+
+    public InvalidInputException(String message) { super(message); }
 }
