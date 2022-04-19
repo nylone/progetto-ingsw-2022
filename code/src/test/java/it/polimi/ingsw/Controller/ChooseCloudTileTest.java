5b11520adf388253212c7632a98906f5652df677
Arianna Salerno <ariannasalerno00@icloud.com>
2022-04-19 21:26:59 +0200

    partially tested ChooseCloudTile

---
+++ code/src/test/java/it/polimi/ingsw/Controller/ChooseCloudTileTest.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.Controller;
+
+import it.polimi.ingsw.Misc.Utils;
+import it.polimi.ingsw.Model.Enums.GameMode;
+import it.polimi.ingsw.Model.GameBoard;
+import it.polimi.ingsw.Model.PlayerBoard;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChooseCloudTileTest {
+
+    @Test
+    public void playerCanTakeStudentsFromCloud() {
+        // arrange
+        GameBoard gameBoard = new GameBoard(GameMode.SIMPLE, "ale", "teo");
+        PlayerBoard currentPlayer = gameBoard.getTurnOrder().getCurrentPlayer();
+        int selectedCloud = Utils.random(gameBoard.getClouds()).getId();
+        ChooseCloudTile action = new ChooseCloudTile(currentPlayer.getId(), selectedCloud);
+        currentPlayer.getEntranceStudents().remove(6);
+        currentPlayer.getEntranceStudents().remove(5);
+        currentPlayer.getEntranceStudents().remove(4);
+        // act
+        action.unsafeExecute(gameBoard);
+        // assert
+        assertTrue(gameBoard.getClouds().get(selectedCloud).getContents().size() == 0);
+        assertTrue(currentPlayer.getEntranceStudents().size() == 7);
+    }
+
+    @Test
+    public void fullEntranceShouldRaiseException() {
+        // arrange
+        GameBoard gameBoard = new GameBoard(GameMode.SIMPLE, "ale", "teo");
+        PlayerBoard currentPlayer = gameBoard.getTurnOrder().getCurrentPlayer();
+        int selectedCloud = Utils.random(gameBoard.getClouds()).getId();
+        ChooseCloudTile action = new ChooseCloudTile(currentPlayer.getId(), selectedCloud);
+        // act
+        action.unsafeExecute(gameBoard);
+        // assert
+        assertTrue(gameBoard.getClouds().get(selectedCloud).getContents().size() == 3);
+        assertTrue(currentPlayer.getEntranceStudents().size() == 7);
+    }
+
+}
