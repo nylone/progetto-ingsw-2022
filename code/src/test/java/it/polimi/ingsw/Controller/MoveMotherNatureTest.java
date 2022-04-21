5845c9d76aee09b9d1e20ff1bf3dab6dd5ffd7e9
Arianna Salerno <ariannasalerno00@icloud.com>
2022-04-21 12:28:19 +0200

    completely tested MoveMotherNature

---
+++ code/src/test/java/it/polimi/ingsw/Controller/MoveMotherNatureTest.java
@@ -0,0 +1,68 @@
+package it.polimi.ingsw.Controller;
+
+import it.polimi.ingsw.Misc.Utils;
+import it.polimi.ingsw.Model.AssistantCard;
+import it.polimi.ingsw.Model.Enums.GameMode;
+import it.polimi.ingsw.Model.GameBoard;
+import it.polimi.ingsw.Model.IslandGroup;
+import it.polimi.ingsw.Model.PlayerBoard;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoveMotherNatureTest {
+
+    @Test
+    public void motherNatureShouldBeMoved() {
+        // arrange
+        GameBoard gameBoard = new GameBoard(GameMode.SIMPLE, "ale", "teo");
+        PlayerBoard player = gameBoard.getTurnOrder().getCurrentPlayer();
+        int randomMovement = new Random().nextInt(1, 7);
+        IslandGroup initialPosition = gameBoard.getIslandField().getMotherNaturePosition();
+        MoveMotherNature action = new MoveMotherNature(player.getId(), randomMovement);
+        // act
+        action.unsafeExecute(gameBoard);
+        // assert
+        assertEquals(Utils.modularSelection(initialPosition, gameBoard.getIslandField().getGroups(), randomMovement),
+                gameBoard.getIslandField().getMotherNaturePosition());
+    }
+
+    @Test
+    public void playerCanMoveAccordingToChosenCard() {
+        // arrange
+        GameHandler gh = new GameHandler(GameMode.SIMPLE);
+        GameBoard gameBoard = new GameBoard(GameMode.SIMPLE, "ale", "teo");
+        PlayerBoard player = gameBoard.getTurnOrder().getCurrentPlayer();
+        AssistantCard card = Utils.random(List.of(player.getAssistantCards()));
+        gameBoard.getTurnOrder().setSelectedCard(player, card, player.getAssistantCards());
+        int randomMovement = new Random().nextInt(card.getMaxMovement());
+        randomMovement = randomMovement == 0 ? 1 : randomMovement;
+        IslandGroup initialPosition = gameBoard.getIslandField().getMotherNaturePosition();
+        PlayerAction action = new MoveMotherNature(player.getId(), randomMovement);
+        // act
+        action.safeExecute(gh.getHistory(), gameBoard);
+        // assert
+        assertEquals(Utils.modularSelection(initialPosition, gameBoard.getIslandField().getGroups(), randomMovement),
+                gameBoard.getIslandField().getMotherNaturePosition());
+    }
+
+    @Test
+    public void playerCantMoveMoreThanAllowed() {
+        // arrange
+        GameHandler gh = new GameHandler(GameMode.SIMPLE);
+        GameBoard gameBoard = new GameBoard(GameMode.SIMPLE, "ale", "teo");
+        PlayerBoard player = gameBoard.getTurnOrder().getCurrentPlayer();
+        AssistantCard card = Utils.random(List.of(player.getAssistantCards()));
+        gameBoard.getTurnOrder().setSelectedCard(player, card, player.getAssistantCards());
+        int invalidMovement = card.getMaxMovement() + 1;
+        IslandGroup initialPosition = gameBoard.getIslandField().getMotherNaturePosition();
+        PlayerAction action = new MoveMotherNature(player.getId(), invalidMovement);
+        // act
+        action.safeExecute(gh.getHistory(), gameBoard);
+        // assert
+        assertEquals(initialPosition, gameBoard.getIslandField().getMotherNaturePosition());
+    }
+}
