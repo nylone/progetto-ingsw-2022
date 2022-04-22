bfa7245e05f4ed41f4f4a1c6696ef71b9c66b23d
Arianna Salerno <ariannasalerno00@icloud.com>
2022-04-22 19:09:10 +0200

    completely tested PlayAssistantCard

---
+++ code/src/test/java/it/polimi/ingsw/Controller/PlayAssistantCardTest.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.Controller;
+
+import it.polimi.ingsw.Model.Enums.GameMode;
+import it.polimi.ingsw.Model.GameBoard;
+import it.polimi.ingsw.Model.PlayerBoard;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PlayAssistantCardTest {
+
+    @Test
+    public void cardShouldBeAssociatedToPlayer() {
+        // arrange
+        GameHandler gh = new GameHandler(GameMode.SIMPLE);
+        GameBoard gameBoard = new GameBoard(GameMode.SIMPLE, "ale", "teo");
+        PlayerBoard player = gameBoard.getTurnOrder().getCurrentPlayer();
+        PlayAssistantCard action = new PlayAssistantCard(player.getId(), 3);
+        // act
+        action.safeExecute(gh.getHistory(), gameBoard);
+        // assert
+        assertTrue(player.getAssistantCards()[2].getUsed());
+        for (int i = 0; i < player.getAssistantCards().length; i++) {
+            if (i != 2) assertTrue(!player.getAssistantCards()[i].getUsed());
+        }
+        assertTrue(gameBoard.getTurnOrder().getSelectedCard(player).get().getPriority() == 3);
+    }
+}
