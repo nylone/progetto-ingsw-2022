fa49df126103b4d84304e6e577f4a1d1a01b5bfd
Arianna Salerno <ariannasalerno00@icloud.com>
2022-05-15 22:32:12 +0200

    implemented and tested component for UI composition

--- src/main/java/it/polimi/ingsw/CLI/GameUI.java
+++ src/main/java/it/polimi/ingsw/CLI/GameUI.java
@@ -2,8 +2,10 @@ package it.polimi.ingsw.CLI;
 
 import it.polimi.ingsw.CLI.Components.CloudUI;
 import it.polimi.ingsw.CLI.Components.IslandUI;
+import it.polimi.ingsw.CLI.Components.PlayerBoardUI;
 import it.polimi.ingsw.Exceptions.Container.EmptyContainerException;
 import it.polimi.ingsw.Exceptions.Container.InvalidContainerIndexException;
+import it.polimi.ingsw.Model.Enums.PawnColour;
 import it.polimi.ingsw.Model.GameBoard;
 import it.polimi.ingsw.Model.IslandGroup;
 
@@ -16,10 +18,31 @@ public class GameUI {
         String clouds = CloudUI.draw(ctx);
         for (IslandGroup ig : ctx.getMutableIslandField().getMutableGroups()) {
             String currentIsland = IslandUI.draw(ig, ctx);
-            screen = screen + currentIsland + '\t';
+            screen = screen + currentIsland + "\t".repeat(2); // '\t' is used for horizontal separation between islands and clouds
 
+            // This will print just one line of the clouds UI component
             screen = screen + clouds.substring(0, clouds.indexOf('\n') + 1);
             clouds = clouds.substring(clouds.indexOf('\n') + 1);
+        }
+        // Playerboard sections' titles. Change the argument of repeat() to further separate islands from playerboards
+        screen = screen + "\n".repeat(3) + "Entrance:\t" + "Dining Room:\t" + "Teachers:\t" + "Towers:\t" + "\n";
+
+        String entrance = PlayerBoardUI.drawEntrance(ctx.getMutableTurnOrder().getMutableCurrentPlayer(), ctx);
+        String towers = PlayerBoardUI.drawTowers(ctx.getMutableTurnOrder().getMutableCurrentPlayer(), ctx);
+        for (PawnColour p : PawnColour.values()) {
+            // This will print just one line of the entrance UI component
+            screen = screen + entrance.substring(0, entrance.indexOf('\n'));
+            entrance = entrance.substring(entrance.indexOf('\n') + 1);
+            // This will print one row of the dining room
+            screen = screen + "\t\t\t" + PlayerBoardUI.drawDiningRoomRow(ctx.getMutableTurnOrder().getMutableCurrentPlayer(),
+                    p);
+            // This will print one teacher per row if present
+            screen = screen + "\t" + PlayerBoardUI.drawTeacher(p, ctx.getMutableTurnOrder().getMutableCurrentPlayer(),
+                    ctx);
+
+            screen = screen + "\t\t  " + towers.substring(0, towers.indexOf('\n') + 1);
+            towers = towers.substring(towers.indexOf("\n") + 1);
+
         }
         return screen;
     }
--- src/test/java/it/polimi/ingsw/CLI/GameUITest.java
+++ src/test/java/it/polimi/ingsw/CLI/GameUITest.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.CLI;
 
-
 import it.polimi.ingsw.Exceptions.Container.EmptyContainerException;
+import it.polimi.ingsw.Exceptions.Container.FullContainerException;
 import it.polimi.ingsw.Exceptions.Container.InvalidContainerIndexException;
 import it.polimi.ingsw.Misc.Utils;
 import it.polimi.ingsw.Model.Enums.GameMode;
@@ -17,15 +17,21 @@ import java.util.Random;
 public class GameUITest {
 
     @Test
-    public void shouldRenderScreen() throws InvalidContainerIndexException, EmptyContainerException {
-        GameBoard gb = new GameBoard(GameMode.SIMPLE, "ale", "teo", "ari", "eriantys");
+    public void shouldRenderScreen() throws InvalidContainerIndexException, EmptyContainerException, FullContainerException {
+        GameBoard gb = new GameBoard(GameMode.SIMPLE, "ale", "teo");
         for (IslandGroup ig : gb.getMutableIslandField().getMutableGroups()) {
             for (int i = 0; i < new Random().nextInt(15); i++) {
                 ig.getMutableIslands().get(0).addStudent(Utils.random(List.of(PawnColour.values())));
             }
             ig.swapTower(gb.getTeamMap().getMutableTowerStorage(Utils.random(List.of(TeamID.ONE, TeamID.TWO))));
         }
+        for (PawnColour p : PawnColour.values()) {
+            for (int i = 0; i < new Random().nextInt(9); i++) {
+                gb.addStudentToDiningRoom(p, gb.getMutableTurnOrder().getMutableCurrentPlayer());
+            }
+        }
         Utils.random(gb.getClouds()).extractContents();
+        gb.getMutableTurnOrder().getMutableCurrentPlayer().removeStudentFromEntrance(4);
         System.out.println(GameUI.draw(gb));
     }
 
