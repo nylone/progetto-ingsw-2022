aaa80e9e0811a48aa8b11aee3154513a2d60def7
Arianna Salerno <ariannasalerno00@icloud.com>
2022-05-15 22:29:06 +0200

    improved indexing of clouds' title

--- src/main/java/it/polimi/ingsw/CLI/Components/CloudUI.java
+++ src/main/java/it/polimi/ingsw/CLI/Components/CloudUI.java
@@ -24,15 +24,15 @@ public class CloudUI {
                             students = students + "\t\t\n";
                         }
                     }
-                    twoClouds = twoClouds + "Nuvola " + c.getId() + "\n" + students;
+                    twoClouds = twoClouds + "Nuvola " + (c.getId()+1) + "\n" + students;
                 }
                 clouds = twoClouds + "\n\n\n";
             }
 
             case 3 -> {
                 String threeClouds = "";
-                threeClouds = threeClouds + "Nuvola " + ctx.getClouds().get(0).getId() + "\t"
-                        + "Nuvola " + ctx.getClouds().get(1).getId() + "\n";
+                threeClouds = threeClouds + "Nuvola " + (ctx.getClouds().get(0).getId()+1) + "\t"
+                        + "Nuvola " + (ctx.getClouds().get(1).getId()+1) + "\n";
                 for (int i = 0; i < 3; i++) {
                     threeClouds = threeClouds +
                             (ctx.getClouds().get(0).getContents().size() > 0 ?
@@ -43,7 +43,7 @@ public class CloudUI {
                                     Symbols.colorizeStudent(ctx.getClouds().get(1).getContents().get(i), "\t" +
                                             Symbols.PAWN) + "\n" : "\t\t\n");
                 }
-                threeClouds = threeClouds + "Nuvola " + ctx.getClouds().get(2).getId() + "\n";
+                threeClouds = threeClouds + "Nuvola " + (ctx.getClouds().get(2).getId()+1) + "\n";
 
                 for (int i = 0; i < 3; i++) {
                     threeClouds = threeClouds +
@@ -57,8 +57,8 @@ public class CloudUI {
 
             case 4 -> {
                 String fourClouds = "";
-                fourClouds = fourClouds + "Nuvola " + ctx.getClouds().get(0).getId() + "\t"
-                        + "Nuvola " + ctx.getClouds().get(1).getId() + "\n";
+                fourClouds = fourClouds + "Nuvola " + (ctx.getClouds().get(0).getId()+1) + "\t"
+                        + "Nuvola " + (ctx.getClouds().get(1).getId()+1) + "\n";
                 for (int i = 0; i < 3; i++) {
                     fourClouds = fourClouds +
                             (ctx.getClouds().get(0).getContents().size() > 0 ?
@@ -69,8 +69,8 @@ public class CloudUI {
                                     Symbols.colorizeStudent(ctx.getClouds().get(1).getContents().get(i), "\t"
                                     + Symbols.PAWN) + "\n" : "\t\t\n");
                 }
-                fourClouds = fourClouds + "Nuvola " + ctx.getClouds().get(2).getId() + "\t"
-                        + "Nuvola " + ctx.getClouds().get(3).getId() + "\n";
+                fourClouds = fourClouds + "Nuvola " + (ctx.getClouds().get(2).getId()+1) + "\t"
+                        + "Nuvola " + (ctx.getClouds().get(3).getId()+1) + "\n";
                 for (int i = 0; i < 3; i++) {
                     fourClouds = fourClouds +
                             (ctx.getClouds().get(2).getContents().size() > 0 ?
