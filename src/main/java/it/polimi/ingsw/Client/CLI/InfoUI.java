7d75eddf3ccebebfc674eb64f8907ae7ff663a3b
Arianna Salerno <ariannasalerno00@icloud.com>
2022-05-21 09:58:13 +0200

    improved InfoUI component
    now if you are the next player you will be better notified

--- src/main/java/it/polimi/ingsw/Client/CLI/InfoUI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/InfoUI.java
@@ -10,15 +10,24 @@ public class InfoUI {
         if(caller.equals(ctx.getMutableTurnOrder().getMutableCurrentPlayer().getNickname()))
             info = info + ctx.getMutableTurnOrder().getMutableCurrentPlayer().getNickname() + " is your turn!\n";
         else
-            info = info + "It's "+ctx.getMutableTurnOrder().getMutableCurrentPlayer().getNickname() + "'s Turn\n";
+            info = info + "It's " + ctx.getMutableTurnOrder().getMutableCurrentPlayer().getNickname() + "'s turn\n";
         if (ctx.getMutableTurnOrder().getCurrentTurnOrder()
                 .indexOf(ctx.getMutableTurnOrder().getMutableCurrentPlayer()) <
                 ctx.getMutableTurnOrder().getCurrentTurnOrder().size() - 1) {
             String nextPlayer = ctx.getMutableTurnOrder().getCurrentTurnOrder().get(ctx.getMutableTurnOrder()
                     .getCurrentTurnOrder().indexOf(ctx.getMutableTurnOrder().getMutableCurrentPlayer()) + 1).getNickname();
-            info = info + "Next player: " + nextPlayer + "\n";
+            if (caller.equals(nextPlayer)) {
+                info = info + "Next player: you \n";
+            }
+            else info = info + "Next player: " + nextPlayer + "\n";
         }
-        else info = info + "Next player: " + ctx.getMutableTurnOrder().getCurrentTurnOrder().get(0).getNickname() + "\n";
+        else {
+            String nextPlayer = ctx.getMutableTurnOrder().getCurrentTurnOrder().get(0).getNickname();
+            if (caller.equals(nextPlayer)) {
+                info = info + "Next player: you \n";
+            }
+            else info = info + "Next player: " + nextPlayer + "\n";
+        }
         if (ctx.getGameMode().equals(GameMode.ADVANCED)) {
             switch (ctx.getCoinReserve()) {
                 case 0 -> info = info + "There are no coins left";
--- src/test/java/it/polimi/ingsw/Client/CLI/InfoUITest.java
+++ src/test/java/it/polimi/ingsw/Client/CLI/InfoUITest.java
@@ -3,17 +3,19 @@ package it.polimi.ingsw.Client.CLI;
 
 import it.polimi.ingsw.Model.Enums.GameMode;
 import it.polimi.ingsw.Model.GameBoard;
+import it.polimi.ingsw.Model.PlayerBoard;
 import org.junit.Test;
 
 public class InfoUITest {
 
     @Test
     public void shouldDrawInfos() {
-       /* GameBoard gb = new GameBoard(GameMode.ADVANCED, "ale", "teo");
-        System.out.println(InfoUI.draw(gb));
+        GameBoard gb = new GameBoard(GameMode.ADVANCED, "ale", "teo");
+        PlayerBoard player = gb.getMutableTurnOrder().getMutableCurrentPlayer();
+        System.out.println(InfoUI.draw(gb, player.getNickname()));
         gb.getMutableTurnOrder().stepToNextPlayer();
         gb.addCoinToReserve(1);
-        System.out.println(InfoUI.draw(gb));*/
+        System.out.println(InfoUI.draw(gb, player.getNickname()));
     }
 
 }
