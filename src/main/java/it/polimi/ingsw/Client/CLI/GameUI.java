2aabc889dd63a10997e8767a51f32ad8978506b8
Arianna Salerno <ariannasalerno00@icloud.com>
2022-05-25 22:28:45 +0200

    followed warning from IDE

--- src/main/java/it/polimi/ingsw/Client/CLI/GameUI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/GameUI.java
@@ -26,11 +26,11 @@ public class GameUI {
 
         String clouds = CloudUI.draw(ctx); // draws the cloud component which will be stripped and printed line by line
         int groupsSize = ctx.getMutableIslandField().getMutableGroups().size();
-        int rows = groupsSize < 8 ? 8 : groupsSize;
         // The number of rows of the UI component depends on the number of island groups.
         // If there are less island groups than the number of CloudUI's lines, padding should be added in the form of
         // empty rows to replace the IslandUI component.
         // The constant '8' is the minimum number of lines/IslandUIs needed to correctly display the cloud component.
+        int rows = Math.max(groupsSize, 8);
         for (int i = 0; i < rows; i++) { // on each row will be printed an IslandUI and one line of CloudUI
             // on every line prints the island group but if there are none to print, it fills the space with
             // an empty line
