f29afbc4a9485ec65b2395cfda172282d318265a
Arianna Salerno <ariannasalerno00@icloud.com>
2022-05-25 22:26:22 +0200

    added documentation and comments

--- src/main/java/it/polimi/ingsw/Client/CLI/CloudUI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/CloudUI.java
@@ -5,35 +5,55 @@ import it.polimi.ingsw.Model.Cloud;
 import it.polimi.ingsw.Model.Enums.PawnColour;
 import it.polimi.ingsw.Model.GameBoard;
 
+/**
+ * CloudUI is a graphical representation (as a String data structure) useful to print multiple cloud components
+ * on the console.
+ * <br>
+ * The internal information of the model element is described in {@link it.polimi.ingsw.Model.Cloud}.
+ */
 public class CloudUI {
 
+    /**
+     * The {@link it.polimi.ingsw.Model.Cloud} will be represented with a title and its related id,
+     * and the contained students if present.
+     * According to the number of players, it will create a different layout of the clouds on the console.
+     *
+     * @param ctx is a reference to the model used to access the clouds' information.
+     * @return the representation of the clouds group.
+     */
     public static String draw(GameBoard ctx) {
         String clouds = "";
         switch (ctx.getClouds().size()) {
-            case 2 -> {
+            case 2 -> { // two clouds will be print on top of each other
                 String twoClouds = "";
                 for (Cloud c : ctx.getClouds()) {
                     String students = "";
 
+                    // Prints one line for every student on the cloud, and aligns it centrally
                     for (PawnColour p : c.getContents()) {
                         students = students + Symbols.colorizeStudent(p, "\t" + Symbols.PAWN);
                         students = students + "\t\n";
                     }
+                    // Handles the empty cloud case
                     if (students.equals("")) {
                         for (int i = 0; i < 3; i++) {
                             students = students + "\t\t\n";
                         }
                     }
+                    // Appends the current cloud in the loop.
+                    // Adds the current cloud title on a line.
+                    // Adds the content on the cloud on separate lines.
                     twoClouds = twoClouds + "\tCloud " + (c.getId() + 1) + "\n" + students;
                 }
                 clouds = twoClouds + "\n\n\n";
             }
 
-            case 3 -> {
+            case 3 -> { // three clouds will be print: the first pair next to each other, the third under the first
                 String threeClouds = "";
-                threeClouds = threeClouds + "Cloud " + (ctx.getClouds().get(0).getId() + 1) + "\t\t"
-                        + "Cloud " + (ctx.getClouds().get(1).getId() + 1) + "\n";
-                for (int i = 0; i < 3; i++) {
+                threeClouds = threeClouds + "Cloud " + (ctx.getClouds().get(0).getId() + 1) // title of cloud 1
+                        + "\t\t" // divider between clouds
+                        + "Cloud " + (ctx.getClouds().get(1).getId() + 1) + "\n"; // title of cloud 2
+                for (int i = 0; i < 3; i++) { // prints the content of the I and II clouds next to each other if present
                     threeClouds = threeClouds +
                             (ctx.getClouds().get(0).getContents().size() > 0 ?
                                     Symbols.colorizeStudent(ctx.getClouds().get(0).getContents().get(i), "\t" +
@@ -43,9 +63,8 @@ public class CloudUI {
                                     Symbols.colorizeStudent(ctx.getClouds().get(1).getContents().get(i), "\t" +
                                             Symbols.PAWN) + "\n" : "\t\t\n");
                 }
-                threeClouds = threeClouds + "Cloud " + (ctx.getClouds().get(2).getId() + 1) + "\n";
-
-                for (int i = 0; i < 3; i++) {
+                threeClouds = threeClouds + "Cloud " + (ctx.getClouds().get(2).getId() + 1) + "\n"; // title of cloud 3
+                for (int i = 0; i < 3; i++) { // prints the content of the III cloud if present
                     threeClouds = threeClouds +
                             (ctx.getClouds().get(2).getContents().size() > 0 ?
                                     Symbols.colorizeStudent(ctx.getClouds().get(2).getContents().get(i), "\t"
@@ -55,11 +74,12 @@ public class CloudUI {
                 clouds = threeClouds + "\n\n\n";
             }
 
-            case 4 -> {
+            case 4 -> { // four clouds will be print as a grid
                 String fourClouds = "";
-                fourClouds = fourClouds + "Cloud " + (ctx.getClouds().get(0).getId() + 1) + "\t\t"
-                        + "Cloud " + (ctx.getClouds().get(1).getId() + 1) + "\n";
-                for (int i = 0; i < 3; i++) {
+                fourClouds = fourClouds + "Cloud " + (ctx.getClouds().get(0).getId() + 1) // title of cloud 1
+                        + "\t\t" // separation between clouds
+                        + "Cloud " + (ctx.getClouds().get(1).getId() + 1) + "\n"; // title of cloud 2
+                for (int i = 0; i < 3; i++) { // prints the content of the I and II clouds next to each other if present
                     fourClouds = fourClouds +
                             (ctx.getClouds().get(0).getContents().size() > 0 ?
                                     Symbols.colorizeStudent(ctx.getClouds().get(0).getContents().get(i), "\t"
@@ -69,9 +89,10 @@ public class CloudUI {
                                     Symbols.colorizeStudent(ctx.getClouds().get(1).getContents().get(i), "\t"
                                             + Symbols.PAWN) + "\n" : "\t\t\n");
                 }
-                fourClouds = fourClouds + "Cloud " + (ctx.getClouds().get(2).getId() + 1) + "\t\t"
-                        + "Cloud " + (ctx.getClouds().get(3).getId() + 1) + "\n";
-                for (int i = 0; i < 3; i++) {
+                fourClouds = fourClouds + "Cloud " + (ctx.getClouds().get(2).getId() + 1) // title of cloud 3
+                        + "\t\t" // separation between clouds
+                        + "Cloud " + (ctx.getClouds().get(3).getId() + 1) + "\n"; // title of cloud 4
+                for (int i = 0; i < 3; i++) { // prints the content of the III and IV clouds next to each other if present
                     fourClouds = fourClouds +
                             (ctx.getClouds().get(2).getContents().size() > 0 ?
                                     Symbols.colorizeStudent(ctx.getClouds().get(2).getContents().get(i), "\t"
--- src/main/java/it/polimi/ingsw/Client/CLI/GameUI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/GameUI.java
@@ -5,16 +5,39 @@ import it.polimi.ingsw.Exceptions.Container.InvalidContainerIndexException;
 import it.polimi.ingsw.Misc.Symbols;
 import it.polimi.ingsw.Model.GameBoard;
 
+/**
+ * GameUI is a graphical representation (as a String data structure)
+ * useful to print {@link it.polimi.ingsw.Client.CLI.CloudUI}
+ * and multiple {@link it.polimi.ingsw.Client.CLI.IslandUI} components
+ * next to each other on the console.
+ */
 public class GameUI {
 
     public static String draw(GameBoard ctx) throws InvalidContainerIndexException, EmptyContainerException {
+    /**
+     * The console will be populated with all the island groups in the island field followed by the clouds on
+     * the same multiline block.
+     *
+     * @param ctx is a reference to the model used to access the {@link it.polimi.ingsw.Model.IslandField} to print
+     *            the {@link it.polimi.ingsw.Client.CLI.IslandUI} and passed to the
+     *            {@link it.polimi.ingsw.Client.CLI.CloudUI}
+     * @return a composed view of islands and clouds
+     */
+    public static String draw(GameBoard ctx) {
+        // The background colour helpful to enhance contrast between black towers and the default black background console
         String screen = Symbols.BACKGROUND;
 
         int line = 0;
-        String clouds = CloudUI.draw(ctx);
+        String clouds = CloudUI.draw(ctx); // draws the cloud component which will be stripped and printed line by line
         int groupsSize = ctx.getMutableIslandField().getMutableGroups().size();
         int rows = groupsSize < 8 ? 8 : groupsSize;
-        for (int i = 0; i < rows; i++) {
+        // The number of rows of the UI component depends on the number of island groups.
+        // If there are less island groups than the number of CloudUI's lines, padding should be added in the form of
+        // empty rows to replace the IslandUI component.
+        // The constant '8' is the minimum number of lines/IslandUIs needed to correctly display the cloud component.
+        for (int i = 0; i < rows; i++) { // on each row will be printed an IslandUI and one line of CloudUI
+            // on every line prints the island group but if there are none to print, it fills the space with
+            // an empty line
             if (i < ctx.getMutableIslandField().getMutableGroups().size()) {
                 String currentIsland = IslandUI.draw(ctx.getMutableIslandField().getMutableGroups().get(i), ctx);
                 screen = screen + currentIsland + "\t".repeat(2); // '\t' is used for horizontal separation between islands and clouds
