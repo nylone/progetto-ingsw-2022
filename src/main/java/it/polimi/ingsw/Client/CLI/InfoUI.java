3e12751894342a816b9ae28c5006ec5210033560
Arianna Salerno <ariannasalerno00@icloud.com>
2022-05-26 22:14:40 +0200

    added documentation and comments

--- src/main/java/it/polimi/ingsw/Client/CLI/InfoUI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/InfoUI.java
@@ -2,29 +2,59 @@ package it.polimi.ingsw.Client.CLI;
 
 import it.polimi.ingsw.Model.Enums.GameMode;
 import it.polimi.ingsw.Model.GameBoard;
+import it.polimi.ingsw.Model.PlayerBoard;
+import it.polimi.ingsw.Model.StudentBag;
+import it.polimi.ingsw.Model.TurnOrder;
 
+import java.util.List;
+
+/**
+ * InfoUI is a graphical informative component (as a String data structure)
+ * useful to print details about the {@link TurnOrder}
+ * and the {@link StudentBag}
+ */
 public class InfoUI {
 
+    /**
+     * It will provide the players with a summary about the turn order, it will show the current and next players in
+     * line and highlight if you are one of them.
+     * <br>
+     * It also shows the number of remaining coins if the game is in advanced mode.
+     *
+     * @param ctx reference to the model used to access {@link GameBoard#getMutableTurnOrder()}
+     * @param caller the player to whom the information will be displayed. It is used to highlight if you are
+     *               the current or next player
+     * @return a visual description of useful information for the players: turns and coins
+     */
     public static String draw(GameBoard ctx, String caller) {
         String info = "";
-        if (caller.equals(ctx.getMutableTurnOrder().getMutableCurrentPlayer().getNickname()))
-            info = info + ctx.getMutableTurnOrder().getMutableCurrentPlayer().getNickname() + " is your turn!\n";
+        PlayerBoard currentPlayer = ctx.getMutableTurnOrder().getMutableCurrentPlayer();
+        List<PlayerBoard> turnOrder = ctx.getMutableTurnOrder().getCurrentTurnOrder();
+
+        // Prints the current player
+        if (caller.equals(currentPlayer.getNickname())) {
+            // Highlight whether the current player is the person to which the console is shown
+            info = info + currentPlayer.getNickname() + " is your turn!\n";
+        }
         else
-            info = info + "It's " + ctx.getMutableTurnOrder().getMutableCurrentPlayer().getNickname() + "'s turn\n";
-        if (ctx.getMutableTurnOrder().getCurrentTurnOrder()
-                .indexOf(ctx.getMutableTurnOrder().getMutableCurrentPlayer()) <
-                ctx.getMutableTurnOrder().getCurrentTurnOrder().size() - 1) {
-            String nextPlayer = ctx.getMutableTurnOrder().getCurrentTurnOrder().get(ctx.getMutableTurnOrder()
-                    .getCurrentTurnOrder().indexOf(ctx.getMutableTurnOrder().getMutableCurrentPlayer()) + 1).getNickname();
+            info = info + "It's " + currentPlayer.getNickname() + "'s turn\n";
+
+        // Prints the next player
+        if (turnOrder.indexOf(currentPlayer) < turnOrder.size() - 1) {
+            String nextPlayer = turnOrder.get(turnOrder.indexOf(currentPlayer) + 1).getNickname();
             if (caller.equals(nextPlayer)) {
+                // Highlight whether the next player is the person to which the console is shown
                 info = info + "Next player: you \n";
             } else info = info + "Next player: " + nextPlayer + "\n";
         } else {
-            String nextPlayer = ctx.getMutableTurnOrder().getCurrentTurnOrder().get(0).getNickname();
+            // This loops around the list of players to address the fact that the next player after the last one
+            // is the first one in line
+            String nextPlayer = turnOrder.get(0).getNickname(); // the first element in the list of players
             if (caller.equals(nextPlayer)) {
                 info = info + "Next player: you \n";
             } else info = info + "Next player: " + nextPlayer + "\n";
         }
+        // Prints the coins left if the game mode is advanced
         if (ctx.getGameMode().equals(GameMode.ADVANCED)) {
             switch (ctx.getCoinReserve()) {
                 case 0 -> info = info + "There are no coins left";
--- src/main/java/it/polimi/ingsw/Client/CLI/IslandUI.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/IslandUI.java
@@ -9,29 +9,45 @@ import it.polimi.ingsw.Model.IslandGroup;
 import java.util.ArrayList;
 
 /**
- * This UI component should draw a representation of the island including its ID value and the students and towers on it. <br>
- * The students will be ordered by frequency on the island to better highlight the influence. <br>
- * Mother Nature is represented by an asterisk
+ * IslandUI allows to print a placeholder containing an {@link IslandGroup} representation or an empty row
+ * which could be needed if there was previously an IslandGroup on the console which does not exist anymore
+ * in the model
  */
 public class IslandUI {
 
+    /**
+     * The {@link IslandGroup} will be represented with its ID value and the students and towers on it.
+     * <br>
+     * The students will be ordered by frequency on the island to better highlight the influence.
+     * <br>
+     * Mother Nature is represented by an asterisk
+     * <br>
+     * The number of tower is shown via a number
+     * @param i the subject to be represented
+     * @param ctx reference to the model used to identify the presence of the mother nature piece on the island group
+     * @return the representation of an island group.
+     */
     public static String draw(IslandGroup i, GameBoard ctx) {
+        // Adds an identifier if the island contains MotherNature
         String mn = ctx.getMutableIslandField().getMutableMotherNaturePosition().equals(i) ? "*" : "";
 
         String students = "";
+        // Prints each student on the island
         ArrayList<PawnColour> sortedStudents = Utils.sortByFrequency(i.getStudents());
         for (PawnColour p : sortedStudents) {
             students = students + Symbols.colorizeStudent(p, Symbols.PAWN + " ");
         }
 
+        // Adds padding if the current island doesn't hold the most students
         int maxStudents = 0;
         for (IslandGroup ig : ctx.getMutableIslandField().getMutableGroups()) {
             maxStudents = Math.max(maxStudents, ig.getStudents().size());
         }
-
         if (i.getStudents().size() < maxStudents) {
-            students = students + "  ".repeat(maxStudents - i.getStudents().size());
+            students = students + "  ".repeat(maxStudents - i.getStudents().size()); // padding
         }
+
+        // Prints the tower representation accounting for team and quantity
         String tower = "";
         if (i.getTowerColour().isPresent()) {
             switch (i.getTowerColour().get()) {
@@ -44,13 +60,21 @@ public class IslandUI {
 
             }
         } else tower = "\t" + " ".repeat(4);
-        String islandTitle = "Island " + i.getId();
+
+        String islandTitle = "Island " + i.getId(); // island's title and id
         if (i.getMutableNoEntryTiles().size() > 0) {
+            // The title will be crossed out if the island's influence cannot be changed
             islandTitle = Symbols.STRIKED + islandTitle + Symbols.RESET + Symbols.BACKGROUND;
         }
         return mn + islandTitle + ":\t" + students + tower;
     }
 
+    /**
+     * It basically provides the same result as {@link IslandUI#draw(IslandGroup, GameBoard)} but it replaces
+     * all characters with whitespaces (except tabulations)
+     * @param ctx reference to the model used to identify the island group containing the most students
+     * @return an all whitespace's filled String, long enough to fill the IslandUIs' composition
+     */
     public static String drawEmptyRow(GameBoard ctx) {
         String mn = "";
 
