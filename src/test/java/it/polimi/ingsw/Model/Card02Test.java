55168fd1bfd458ccbbd4bd842e1ad3a967a561e3
Arianna Salerno <ariannasalerno00@icloud.com>
2022-06-10 21:55:35 +0200

    added documentation and comments

--- src/test/java/it/polimi/ingsw/Model/Card02Test.java
+++ src/test/java/it/polimi/ingsw/Model/Card02Test.java
@@ -8,24 +8,39 @@ import static org.junit.Assert.assertEquals;
 
 public class Card02Test {
 
+    /**
+     * Character card 02 should leave the teachers control untouched except for the teachers of whom multiple players
+     * hold same influence
+     * @throws Exception is thrown when an invalid card activation happens, but this test should not error out
+     * (by definition)
+     */
     @Test
     public void checkEffectCard02IsWorking() throws Exception {
         // arrange
-        Model gb = new Model(GameMode.ADVANCED, "ale", "teo");
+        Model gb = new Model(GameMode.ADVANCED, "ale", "teo"); // advanced mode needed for character cards
+        // giving the same influence over a common teacher (BLUE) to both players
         gb.getMutablePlayerBoardById(0).addStudentToDiningRoom(PawnColour.BLUE);
         gb.getMutablePlayerBoardById(1).addStudentToDiningRoom(PawnColour.BLUE);
+        // assigning blue teacher to one player
         gb.setTeacher(PawnColour.BLUE, gb.getMutablePlayerBoardByNickname("teo"));
+        // saving information about initial teachers' control situation
         PlayerBoard greenOwner = gb.getTeachers().get(PawnColour.GREEN);
         PlayerBoard pinkOwner = gb.getTeachers().get(PawnColour.PINK);
         PlayerBoard redOwner = gb.getTeachers().get(PawnColour.RED);
         PlayerBoard yellowOwner = gb.getTeachers().get(PawnColour.YELLOW);
 
         Card02 card = new Card02(gb);
+        // creates input to let the player interact with card
         CharacterCardInput activateCardAction = new CharacterCardInput(gb.getMutablePlayerBoardByNickname("ale"));
+
         // act
+        // activates the card to validate teachers control
         card.unsafeApplyEffect(activateCardAction);
+
         // assert
+        // blue teacher should have been assigned to the player who used the card 02
         assertEquals(gb.getMutablePlayerBoardByNickname("ale"), gb.getTeachers().get(PawnColour.BLUE));
+        // the other teachers' control should remain unchanged
         assertEquals(greenOwner, gb.getTeachers().get(PawnColour.GREEN));
         assertEquals(pinkOwner, gb.getTeachers().get(PawnColour.PINK));
         assertEquals(redOwner, gb.getTeachers().get(PawnColour.RED));
--- src/test/java/it/polimi/ingsw/Model/Card03Test.java
+++ src/test/java/it/polimi/ingsw/Model/Card03Test.java
@@ -7,50 +7,96 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * This test set verifies that the card 03 is able to change the control of an island leaving mother nature still
+ */
 public class Card03Test {
-    Model gb = new Model(GameMode.ADVANCED, "ari", "teo");
+    Model gb = new Model(GameMode.ADVANCED, "ari", "teo"); // advanced mode needed for character cards
     Card03 card = new Card03(gb);
 
+    /**
+     * Card 03 should be able to change influence on an island according to rules without moving mother nature on it
+     *
+     * @throws Exception is thrown when an invalid card activation happens, but this test should not error out
+     * (by definition)
+     */
     @Test
     public void checkUse() throws Exception {
+        // arrange
         PlayerBoard pb1 = gb.getMutablePlayerBoardByNickname("ari");
         PlayerBoard pb2 = gb.getMutablePlayerBoardByNickname("teo");
         IslandGroup ig = gb.getMutableIslandField().getMutableIslandGroupById(5);
+        // select an island on which students will be placed
         Island island = ig.getMutableIslands().get(0);
+        // find mother nature position (should remain unchanged)
         IslandGroup expectedMotherNaturePosition = gb.getMutableIslandField().getMutableMotherNaturePosition();
 
+        // adding students to island
         island.addStudent(PawnColour.BLUE);
         island.addStudent(PawnColour.BLUE);
         island.addStudent(PawnColour.BLUE);
         island.addStudent(PawnColour.YELLOW);
 
+        // assigning teachers to players to let first player win
         gb.setTeacher(PawnColour.BLUE, pb1);
         gb.setTeacher(PawnColour.YELLOW, pb2);
 
+        // creates input to let the player interact with card
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
+        // input holds the selected island
         input.setTargetIsland(island);
+
+        // act
+        // activates the card to resolve the influence on the island
         if (card.checkInput(input)) card.unsafeApplyEffect(input);
 
+        // assert
+        // tower colour should reflect influence given to first player
         assertEquals(ig.getTowerColour().get(), gb.getTeamMapper().getMutableTowerStorage(pb1).getColour());
+        // mother nature should still be on the initial island
         assertEquals(expectedMotherNaturePosition, gb.getMutableIslandField().getMutableMotherNaturePosition());
     }
 
+    /**
+     * An invalid player action should error out.
+     * <br>
+     * An island has to be selected for the action to be working
+     *
+     * @throws Exception an invalid input has been used to activate the card
+     */
     @Test(expected = InputValidationException.class)
     public void checkInvalidInput() throws Exception {
+        // creates a wrong input which will not be filled with information
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
         if (card.checkInput(input)) card.unsafeApplyEffect(input);
     }
 
+    /**
+     * An invalid player action should error out.
+     * <br>
+     * An island must have a valid id to be selected
+     *
+     * @throws Exception an invalid input has been used to activate the card
+     */
     @Test(expected = InputValidationException.class)
     public void checkInvalidIslandInput() throws Exception {
+        // creates a wrong input (player will select an invalid island)
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
         Island island = new Island(13);
         input.setTargetIsland(island);
         if (card.checkInput(input)) card.unsafeApplyEffect(input);
     }
 
+    /**
+     * An invalid player action should error out.
+     * <br>
+     * An island must exist in the island field to be selected
+     *
+     * @throws Exception an invalid input has been used to activate the card
+     */
     @Test(expected = InputValidationException.class)
     public void checkIslandNotInField() throws Exception {
+        // creates a wrong input (player will select an invalid island)
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
         Island island = new Island(8);
         input.setTargetIsland(island);
