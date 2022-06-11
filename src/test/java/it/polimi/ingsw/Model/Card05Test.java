d7bcddf66f2f6ca88f88b0ef521e642dfc3fd8d8
Arianna Salerno <ariannasalerno00@icloud.com>
2022-06-11 22:05:37 +0200

    added documentation and comments

--- src/test/java/it/polimi/ingsw/Model/Card05Test.java
+++ src/test/java/it/polimi/ingsw/Model/Card05Test.java
@@ -8,22 +8,44 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
+/**
+ * This test set verifies that character card 05 is able to manage, receive and give no entry tiles.
+ * <br>
+ * It should also be able to understand to which destination the tile is destined or error out
+ */
 public class Card05Test {
-    Model gb = new Model(GameMode.ADVANCED, "ari", "teo");
+    Model gb = new Model(GameMode.ADVANCED, "ari", "teo"); // advanced mode needed for character cards
     Card05 card05 = new Card05(gb);
 
+    /**
+     * Card 05 should ba able to hold 4 entry tile and when activated to put one tile on the chosen island.
+     * If the island doesn't need it anymore the card should be able to receive the tile used
+     *
+     * @throws Exception is thrown when an invalid card activation happens, but this test should not error out
+     * (by definition)
+     */
     @Test
     public void checkUse() throws Exception {
+        // arrange
+        // card should only hold no entry tile items
         assertSame(card05.getStateType(), StateType.NOENTRY);
         PlayerBoard pb = gb.getMutableTurnOrder().getMutableCurrentPlayer();
 
+
+        // creates input to let the current player interact with card
         CharacterCardInput input = new CharacterCardInput(pb);
         IslandField field = gb.getMutableIslandField();
+        // input holds the selected island
         input.setTargetIsland(field.getMutableIslandById(1));
+
+        // act
+        // activates the card to resolve the influence on the island
         if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
         assertEquals(3, card05.getState().size()); // 3 tiles left after one use
-        assertEquals(1, field.getMutableIslandGroupById(1).getMutableNoEntryTiles().size()); // the island group contains the NoEntryTile
+        // the island group contains the NoEntryTile
+        assertEquals(1, field.getMutableIslandGroupById(1).getMutableNoEntryTiles().size());
 
+        // simulate mother nature effect on island and returning tile to card 05
         field.getMutableIslandGroupById(1).resetNoEntry();
 
         // force return the NoEntryTile
@@ -31,43 +53,73 @@ public class Card05Test {
         assertEquals(4, card05.getState().size()); // 4 tiles left after the return of the NoEntryTile
     }
 
+    /**
+     * An invalid player action should error out.
+     * <br>
+     * An island has to be selected for the action to be working
+     *
+     * @throws Exception an invalid input has been used to activate the card
+     */
     @Test(expected = InputValidationException.class)
     public void checkInputException() throws Exception {
+        // creates a wrong input which will not be filled with information
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
         if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
     }
 
+    /**
+     * An invalid player action should error out.
+     * <br>
+     * An island must have a valid id to be selected
+     *
+     * @throws Exception an invalid input has been used to activate the card
+     */
     @Test(expected = InputValidationException.class)
     public void checkInvalidIslandInput() throws Exception {
+        // creates a wrong input (player will select an invalid island)
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
         Island island = new Island(13);
         input.setTargetIsland(island);
         if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
     }
 
+    /**
+     * An invalid player action should error out.
+     * <br>
+     * An island must exist in the island field to be selected
+     *
+     * @throws Exception an invalid input has been used to activate the card
+     */
     @Test(expected = InputValidationException.class)
     public void checkIslandNotInField() throws Exception {
+        // creates a wrong input (player will select an invalid island)
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
         Island island = new Island(8);
         input.setTargetIsland(island);
         if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
     }
 
+    /**
+     * If the card has already given out all of its tiles, it should error out when prompted for handling out more
+     *
+     * @throws Exception
+     */
     @Test(expected = InputValidationException.class)
     public void checkEmptyCard() throws Exception {
+        // arrange
         PlayerBoard pb = gb.getMutableTurnOrder().getMutableCurrentPlayer();
+        // creates input to let the current player interact with card
         CharacterCardInput input = new CharacterCardInput(pb);
         IslandField field = gb.getMutableIslandField();
+        // input holds the selected island
         input.setTargetIsland(field.getMutableIslandById(1));
-        if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
-        input.setTargetIsland(field.getMutableIslandById(1));
-        if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
-        input.setTargetIsland(field.getMutableIslandById(1));
-        if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
-        input.setTargetIsland(field.getMutableIslandById(1));
-        if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
+        // emptying card of all of its entry tiles
+        for (int i = 0; i < 4; i++) {
+            if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
+        }
 
-        input.setTargetIsland(field.getMutableIslandById(1));
+        // act
+        // trying to use card when it is empty
         if (card05.checkInput(input)) card05.unsafeApplyEffect(input);
     }
 }
--- src/test/java/it/polimi/ingsw/Model/Card06And08Test.java
+++ src/test/java/it/polimi/ingsw/Model/Card06And08Test.java
@@ -8,20 +8,40 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Card06And08Test {
-    Model gb = new Model(GameMode.ADVANCED, "ari", "teo");
+    Model gb = new Model(GameMode.ADVANCED, "ari", "teo"); // advanced mode needed for character cards
     Card06 card06 = new Card06(gb);
     Card08 card08 = new Card08(gb);
 
+    /**
+     * Card 06 should be able to set a flag in the model context
+     *
+     * @throws Exception @throws Exception is thrown when an invalid card activation happens, but this test should not error out
+     * (by definition)
+     */
     @Test
     public void checkUse06() throws Exception {
+        // arrange
+        // creates input to let the current player interact with card
         CharacterCardInput input = new CharacterCardInput(gb.getMutableTurnOrder().getMutableCurrentPlayer());
+
+        // act
+        // activates the card to resolve the influence on the island
         if (card06.checkInput(input)) card06.unsafeApplyEffect(input);
 
+        // assert
+        // flag should be true
         assertTrue(gb.getMutableEffects().isTowerInfluenceDenied());
     }
 
+    /**
+     * Card 08 should be able to give 2 more influence points when activated
+     *
+     * @throws Exception @throws Exception is thrown when an invalid card activation happens, but this test should not error out
+     * (by definition)
+     */
     @Test
     public void checkUse08() throws Exception {
+        // arrange
         Island island = gb.getMutableIslandField().getMutableIslandGroupById(1).getMutableIslands().get(0);
         IslandGroup islandGroup = gb.getMutableIslandField().getMutableIslandGroupById(0);
         PlayerBoard pb2 = gb.getMutablePlayerBoardByNickname("teo");
@@ -29,9 +49,11 @@ public class Card06And08Test {
         island.addStudent(PawnColour.RED);
         gb.setTeacher(PawnColour.RED, pb2);
 
-        turnOrder.setSelectedCard(turnOrder.getMutableCurrentPlayer(), turnOrder.getMutableCurrentPlayer().getMutableAssistantCards().get(1));
+        turnOrder.setSelectedCard(turnOrder.getMutableCurrentPlayer(),
+                turnOrder.getMutableCurrentPlayer().getMutableAssistantCards().get(1));
         turnOrder.stepToNextPlayer();
-        turnOrder.setSelectedCard(turnOrder.getMutableCurrentPlayer(), turnOrder.getMutableCurrentPlayer().getMutableAssistantCards().get(0));
+        turnOrder.setSelectedCard(turnOrder.getMutableCurrentPlayer(),
+                turnOrder.getMutableCurrentPlayer().getMutableAssistantCards().get(0));
         turnOrder.stepToNextPlayer();
 
         CharacterCardInput input = new CharacterCardInput(turnOrder.getMutableCurrentPlayer());
