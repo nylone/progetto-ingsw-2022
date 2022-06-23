9d1614a1b6b5622a7b4ef97eb4397a814c64ff77
rampeo <dev@rampeo.com>
2022-06-23 16:54:01 +0200

    added arguments interpretation, removed default logging (now on demand)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,65 @@
 import it.polimi.ingsw.Client.CLI.CLI;
 import it.polimi.ingsw.Client.GUI.GUI;
+import it.polimi.ingsw.Logger;
 import it.polimi.ingsw.Server.WelcomeServer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
 
 public class Main {
     public static void main(String... args) throws IOException {
+        if (args.length >= 1) {
+            if (args.length > 1 && args[1].trim().equalsIgnoreCase("-d")) {
+                Logger.enable(true);
+            }
+            switch (args[0].trim().toLowerCase()) {
+                case "s" -> {
+                    new Thread(new WelcomeServer()).start();
+                    return;
+                }
+                case "c" -> {
+                    CLI.main();
+                    return;
+                }
+                case "g" -> {
+                    GUI.main();
+                    return;
+                }
+                case default -> Logger.info("Wrong input, redirecting to main menu...");
+            }
+        }
         System.out.println("""
-                Welcome to the Eriantys startup tool!
-                Please choose one of the following options:
-                [S]\tstart server
-                [c]\tstart CLI client
-                [g]\tstart GUI client
-                                
-                [q]\tquit this tool
-                """);
+            Welcome to the Eriantys startup tool!
+            Please choose one of the following options:
+            [S]\tstart server
+            [c]\tstart CLI client
+            [g]\tstart GUI client
+            
+            [d]\tenable the logging service for debugging purposes
+                            
+            [q]\tquit this tool
+            
+            Note: next time you run this tool, you can use the same [s/c/g] characters as command line parameters to skip this menu.
+            Note: you can enable the logging service via cli parameters by appending a [-d] at the end of the arguments.
+            Example: "java --enable-preview -jar <program-name>.jar s -d" will start the server in debug mode.
+            """);
 
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
         boolean again = true;
         do {
             // read user input
-            String input = reader.readLine().trim().toLowerCase();
+            String input;
+            input = reader.readLine().trim().toLowerCase();
+            input = input.substring(0, input.length() > 0 ? 1 : 0);
             // handle input
-            switch (input.substring(0, input.length() > 0 ? 1 : 0)) {
+            switch (input) {
+                case "d" -> {
+                    Logger.enable(true);
+                    System.out.println("Logging enabled.");
+                }
                 case "s", "" -> {
                     System.out.println("Starting Server...");
                     new Thread(new WelcomeServer()).start();
@@ -42,7 +76,7 @@ public class Main {
                     again = false;
                 }
                 case "q" -> {
-                    System.out.println("Quitting Startup tool...");
+                    System.out.println("Quitting the startup tool...");
                     again = false;
                 }
                 case default -> System.out.println("Wrong input, please try again.");
--- src/main/java/it/polimi/ingsw/Logger.java
+++ src/main/java/it/polimi/ingsw/Logger.java
@@ -3,19 +3,21 @@ package it.polimi.ingsw;
 import java.util.logging.Level;
 
 public class Logger {
+    private static boolean enabled = false;
+
     public static void info(String message) {
-        java.util.logging.Logger.getLogger(Logger.class.getPackageName()).info(message);
+        if (enabled) java.util.logging.Logger.getLogger(Logger.class.getPackageName()).info(message);
     }
 
     public static void warning(String message) {
-        java.util.logging.Logger.getLogger(Logger.class.getPackageName()).warning(message);
+        if (enabled) java.util.logging.Logger.getLogger(Logger.class.getPackageName()).warning(message);
     }
 
     public static void severe(String message) {
-        java.util.logging.Logger.getLogger(Logger.class.getPackageName()).severe(message);
+        if (enabled) java.util.logging.Logger.getLogger(Logger.class.getPackageName()).severe(message);
     }
 
-    public static void setLevel(Level level) {
-        java.util.logging.Logger.getLogger(Logger.class.getPackageName()).setLevel(level);
+    public static void enable(boolean enabled) {
+        Logger.enabled = enabled;
     }
 }
