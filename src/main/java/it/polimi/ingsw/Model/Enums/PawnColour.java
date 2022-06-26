1124d56efc7f3f4642d1540a418f7eeb936aa788
Arianna Salerno <ariannasalerno00@icloud.com>
2022-06-26 11:41:41 +0200

    fixed to match dining room row order

--- src/main/java/it/polimi/ingsw/Model/Enums/PawnColour.java
+++ src/main/java/it/polimi/ingsw/Model/Enums/PawnColour.java
@@ -4,11 +4,11 @@ import java.io.Serial;
 import java.io.Serializable;
 
 public enum PawnColour implements Serializable {
-    BLUE,
     GREEN,
-    PINK,
     RED,
-    YELLOW;
+    YELLOW,
+    PINK,
+    BLUE;
     @Serial
     private static final long serialVersionUID = 125L; // convention: 1 for model, (01 -> 99) for objects
 
