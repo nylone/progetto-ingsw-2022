89f24be4c652cec11da9d4df070c19087e0c97f2
rampeo <dev@rampeo.com>
2022-06-29 03:20:28 +0200

    documented studentbag

--- src/main/java/it/polimi/ingsw/Model/StudentBag.java
+++ src/main/java/it/polimi/ingsw/Model/StudentBag.java
@@ -9,12 +9,19 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The StudentBag contains a limited number of {@link PawnColour} ordered randomly.
+ */
 public class StudentBag implements Serializable {
     @Serial
     private static final long serialVersionUID = 130L; // convention: 1 for model, (01 -> 99) for objects
     private ArrayList<PawnColour> studentBag;
     private boolean isEmpty;
 
+    /**
+     * Construct the bag
+     * @param numOfStudentsPerColour the number of each color of students to put in the bag
+     */
     public StudentBag(int numOfStudentsPerColour) {
         this.studentBag = new ArrayList<>(numOfStudentsPerColour * PawnColour.values().length);
         for (PawnColour colour :
@@ -27,10 +34,20 @@ public class StudentBag implements Serializable {
         this.isEmpty = false;
     }
 
+    /**
+     * Check to see if the bag is empty
+     * @return true if the bag is empty
+     */
     public boolean isEmpty() {
         return this.isEmpty;
     }
 
+    /**
+     * Extract multiple {@link PawnColour}s at once
+     * @param extractions number of maximum extractions to carry out. The number of extracted students may be lower than the
+     *                    number specified as input, if the bag empties out during the extraction
+     * @return an Unmodifiable {@link List} containing the extracted {@link PawnColour}
+     */
     public List<PawnColour> multipleExtraction(int extractions) {
         List<PawnColour> extracted = new ArrayList<>();
         for (int i = 0; i < extractions && !this.isEmpty; i++) {
@@ -41,25 +58,42 @@ public class StudentBag implements Serializable {
                 throw new RuntimeException(e);
             }
         }
-        return extracted;
+        return List.copyOf(extracted);
     }
 
+    /**
+     * Extract a single {@link PawnColour} from the bag
+     * @return the extracted student
+     * @throws EmptyContainerException if the bag is empty
+     */
     public PawnColour extract() throws EmptyContainerException {
         if (this.isEmpty) throw new EmptyContainerException("StudentBag");
         if (this.getSize() == 1) this.isEmpty = true;
         return this.studentBag.remove(this.studentBag.size() - 1);
     }
 
+    /**
+     * Check the remaining students in the bag
+     * @return the size of the bag
+     */
     public int getSize() {
         return studentBag.size();
     }
 
+    /**
+     * Put a student back in the bag and shuffle it in.
+     * @param colour the {@link PawnColour} to add back to the bag
+     */
     public void appendAndShuffle(PawnColour colour) {
         if (this.getSize() == 0) this.isEmpty = false;
         this.studentBag.add(colour);
         Utils.shuffle(this.studentBag);
     }
 
+    /**
+     * Used to sanitize {@link Model}. Removes information about the internal contents of the bag, making the spoofing of
+     * technically hidden information impossible on the client side.
+     */
     public void removeContentReference() {
         this.studentBag = null;
     }
