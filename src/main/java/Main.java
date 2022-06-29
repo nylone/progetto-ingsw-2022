8ae622e32fceaa4559b444377d565d57d0f8d99b
rampeo <dev@rampeo.com>
2022-06-29 03:40:48 +0200

    documented main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,14 @@ import it.polimi.ingsw.Server.WelcomeServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+/**
+ * main class of the project, call the {@link #main} function to start the whole thing up
+ */
 public class Main {
+    /**
+     * When fed the proper cli inputs, starts the appropriate elements of the project
+     * @param args
+     */
     public static void main(String... args) {
         if (args.length >= 1) {
             for (String arg : args) {
@@ -14,16 +21,20 @@ public class Main {
                     case "-d" -> Logger.enable(true);
                     case "c" -> {
                         CLI.main();
+                        return;
                     }
                     case "g" -> {
                         GUI.main();
+                        return;
                     }
                     case "s" -> {
                         new Thread(new WelcomeServer()).start();
+                        return;
                     }
                     case String a && a.startsWith("s:") -> {
                         int port = Integer.parseInt(a.substring(2));
                         new Thread(new WelcomeServer(port, new InetSocketAddress(port).getAddress())).start();
+                        return;
                     }
                     case default -> {
                         Logger.info("Wrong input, quitting...");
