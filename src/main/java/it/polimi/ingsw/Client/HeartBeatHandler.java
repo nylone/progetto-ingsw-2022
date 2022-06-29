11df22b2e21786724ba4b1b0d8906ea07064f26f
rampeo <dev@rampeo.com>
2022-06-29 03:36:26 +0200

    documented heart beat handler

--- src/main/java/it/polimi/ingsw/Client/HeartBeatHandler.java
+++ src/main/java/it/polimi/ingsw/Client/HeartBeatHandler.java
@@ -7,8 +7,16 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * The server expects to receive a {@link HeartBeatRequest} at intervals of max 15 seconds. This Handler is responsible
+ * for keeping the connection alive
+ */
 public class HeartBeatHandler {
 
+    /**
+     * Given a socket, keep the connection active
+     * @param socketWrapper the socket to keep alive
+     */
     public static void handle(SocketWrapper socketWrapper) {
         Timer timer = new Timer();
 
