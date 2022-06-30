b72c7877ceda029a6809feb32c1d0b6714a74665
rampeo <dev@rampeo.com>
2022-06-30 19:34:48 +0200

    docs for messages

--- src/main/java/it/polimi/ingsw/Server/Messages/HeartBeatMessage.java
+++ src/main/java/it/polimi/ingsw/Server/Messages/HeartBeatMessage.java
@@ -4,6 +4,9 @@ import it.polimi.ingsw.Server.Messages.Events.Requests.ClientRequest;
 
 import java.io.Serial;
 
+/**
+ * A message used to keep the connection alive, used in {@link it.polimi.ingsw.Network.KeepAliveSocketWrapper}
+ */
 public class HeartBeatMessage extends ClientRequest {
     @Serial
     private static final long serialVersionUID = 357L;
--- src/main/java/it/polimi/ingsw/Server/Messages/Message.java
+++ src/main/java/it/polimi/ingsw/Server/Messages/Message.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.Server.Messages;
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * Represent a serializable entity to be sent over the network
+ */
 public abstract class Message implements Serializable {
     @Serial
     private static final long serialVersionUID = 300L; // convention: 3 for network, (01 -> 99) for objects
